ea1cbc58c5c3a8490a998a030d1273581d781c59
iOS_FuYaoHui <diligencefu@sina.com>
2017-05-25 11:49:57 +0800

    Initial Commit

---
+++ TestForGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestForGit
+//
+//  Created by ChiXin on 2017/5/25.
+//  Copyright © 2017年 ChiXin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
